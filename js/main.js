1f7cddb721850f21723427d1034fa869c3fc51ea
manimurugiah <manikandanmpm6@gmail.com>
2022-12-22 10:48:57 +0530

    add file

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let food={
+    name:"",
+    quality:"",
+    quntity:"",
+    price:"",
+}
+food.name=prompt("Enter the name");
+food.quality=prompt("Enter the quality");
+food.quntity=prompt("Enter the quantity");
+food.price=prompt("Enter the price");
+console.log(food);
